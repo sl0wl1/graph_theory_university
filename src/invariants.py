35dbd1c8d2c214076f668c755360885f40f92e9e
sl0wl1 <arne_preuss@web.de>
2025-01-07 17:00:46 +0100

    implemented vertex and edge count for invariant grouping

--- src/invariants.py
+++ src/invariants.py
@@ -23,3 +23,43 @@ def vertex_degree_invariant(
     reaction_centre_invariant.sort()
 
     return group_centre_invariant, reaction_centre_invariant
+
+
+def vertex_count_invariant(
+    group_centre: nx.Graph, reaction_centre: nx.Graph
+) -> Tuple[List[int], List[int]]:
+    """Implementation for comparing vertex counts of two graphs, used for invariant comparison. Used in group_after_invariant
+
+    Args:
+
+        group_centre (nx.Graph): group centre from your already grouped reactions list
+        reaction_centre (nx.Graph): reaction centre from your reactions list
+
+    Returns:
+        group_centre_invariant, reaction_centre_invariant Tuple[List[int], List[int]]: vertex count list of inputs
+    """
+    group_centre_invariant = [group_centre.number_of_nodes()]
+
+    reaction_centre_invariant = [reaction_centre.number_of_nodes()]
+
+    return group_centre_invariant, reaction_centre_invariant
+
+
+def edge_count_invariant(
+    group_centre: nx.Graph, reaction_centre: nx.Graph
+) -> Tuple[List[int], List[int]]:
+    """Implementation for comparing edge counts of two graphs, used for invariant comparison. Used in group_after_invariant
+
+    Args:
+
+        group_centre (nx.Graph): group centre from your already grouped reactions list
+        reaction_centre (nx.Graph): reaction centre from your reactions list
+
+    Returns:
+        group_centre_invariant, reaction_centre_invariant Tuple[List[int], List[int]]: edge count list of inputs
+    """
+    group_centre_invariant = [group_centre.number_of_edges()]
+
+    reaction_centre_invariant = [reaction_centre.number_of_edges()]
+
+    return group_centre_invariant, reaction_centre_invariant
